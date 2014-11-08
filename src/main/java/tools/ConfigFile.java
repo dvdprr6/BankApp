aad5d59c48554ac6a219a421e666a5a4a4e976ef
David Parr <dvdprr6@gmail.com>
2014-11-08 17:01:24 -0500

    store the configuration information in a hash map

--- src/main/java/tools/ConfigFile.java
+++ src/main/java/tools/ConfigFile.java
@@ -1,6 +1,5 @@
 package tools;
 
-import java.lang.*;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -8,20 +7,39 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 
 public class ConfigFile{
-	private HashMap<String, String> parameterMap;
+	private HashMap<String, String> parameterMap = new HashMap<String, String>();
 	
-	public ConfigFile(String filename) throws IOException{
-		parameterMap = new HashMap<String, String>();
+	public ConfigFile(String filename){
+		String fileAsString = null;
 		
+		try{
+			fileAsString = convertFileIntoString(filename);
+		}catch(IOException io){
+			io.printStackTrace();
+		}
+		
+		String[] lines = fileAsString.split("/n");
+		
+		for(String currentLine : lines){
+			addKeyToHashMap(currentLine.trim());
+		}
+	}
+	
+	private String convertFileIntoString(String filename) throws IOException{
 		byte[] readFile = Files.readAllBytes(Paths.get(filename));
-		String fileAsString = new String(readFile, StandardCharsets.UTF_8);
-		
-		for(String currentLine : fileAsString.split("/n")){
-			String trimCurrentLine = currentLine.trim();
-			
-			if(!trimCurrentLine.startsWith("#")){
-				
+		return new String(readFile, StandardCharsets.UTF_8);
+	}
+	
+	private void addKeyToHashMap(String lineToAdd){
+		if(!lineToAdd.startsWith("#")){
+			String[] keyAndValue = lineToAdd.split("=");
+			if(keyAndValue.length == 2){
+				parameterMap.put(keyAndValue[0].trim(), keyAndValue[1].trim());
 			}
 		}
+	}
+	
+	public String get(String key){
+		return parameterMap.get(key);
 	}
 }
