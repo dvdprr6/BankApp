f67db95a19f784d0b1630c7105eef5094d769a68
David Parr <dvdprr6@gmail.com>
2014-11-15 17:00:55 -0500

    start the server class

---
+++ src/main/java/server/StartServer.java
@@ -0,0 +1,19 @@
+package server;
+
+import java.io.IOException;
+
+
+import web.WebServer;
+
+
+public class StartServer{
+	public static void main(String[] args){
+		String configPath = System.getProperty("user.dir") + "/conf/java/dev/bank.conf";
+		String logPath = System.getProperty("user.dir") + "/logs/server.log";
+		
+		WebServer webserver = new WebServer(configPath, logPath);
+
+		webserver.start();
+		
+	}
+}
