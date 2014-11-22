e5115c9fea274d3a323da30a622318a4ee5de43b
David Parr <dvdprr6@gmail.com>
2014-11-21 22:54:04 -0500

    added YTD Earnings table

--- models/models.py
+++ models/models.py
@@ -16,14 +16,13 @@ class GeneralStatementInfo(Base):
 
 	__tablename__ = 'general_statement_info'
 
-	id = Column(Integer, primary_key=True)
+	id = Column(Integer, primary_key=True, nullable=False)
 	rate = Column(String(55))
 	hours = Column(String(55))
 	company_name = Column(String(255))
 	payment_date = Column(DateTime, default=datetime.datetime.utcnow)
 
-	# earnings_deductions_association = relationship("EarningsDeductionAssociation",
-	# 												cascade="save-update, merge, delete, delete-orphan")
+	# earnings_deductions_association = relationship("EarningsDeductionAssociation", cascade="save-update, merge, delete, delete-orphan")
 
 	def to_dict(self):
 		return{
@@ -32,4 +31,21 @@ class GeneralStatementInfo(Base):
 			'hours':self.hours,
 			'company_name':self.company_name,
 			'payment_date':seld.payment_date
+		}
+
+class YTDEarnings(Base):
+
+	__tablename__ = 'ytd_earnings'
+
+	id = Column(Integer, primary_key=True, nullable=False)
+	ytd_hours = Column(String(55))
+	ytd_amount = Column(String(55))
+
+	# earning_association = relationship("EarningAssociation", cascade="save-update, merge, delete, delete-orphan")
+
+	def to_dict(self):
+		return{
+			'id': self.id,
+			'ytd_hours': self.ytd_hours,
+			'ytd_amount':self.ydt_amount
 		}
