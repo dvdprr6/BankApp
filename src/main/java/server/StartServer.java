ee63a1d25b5016bf3f267ff143d529cea03c75ea
David Parr <dvdprr6@gmail.com>
2014-11-21 22:56:54 -0500

    edit bank configuration file location for java

--- src/main/java/server/StartServer.java
+++ src/main/java/server/StartServer.java
@@ -8,7 +8,7 @@ import web.WebServer;
 
 public class StartServer{
 	public static void main(String[] args){
-		String configPath = System.getProperty("user.dir") + "/conf/java/dev/bank.conf";
+		String configPath = System.getProperty("user.dir") + "/conf/dev/bank.conf";
 		String logPath = System.getProperty("user.dir") + "/logs/server.log";
 		
 		WebServer webserver = new WebServer(configPath, logPath);
