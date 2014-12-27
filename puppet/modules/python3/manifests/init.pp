98b40b6d003e97bee48417617aa7a2da381e57b6
David Parr <dvdprr6@gmail.com>
2014-12-27 18:19:33 -0500

    added the python 3 download

--- puppet/modules/python3/manifests/init.pp
+++ puppet/modules/python3/manifests/init.pp
@@ -3,6 +3,13 @@
 #
 
 class python3{
+	exec{"download-python3":
+		command => 'wget -q http://www.python.org/ftp/python/3.4.2/Python-3.4.2.tar.xz --no-check-certificate',
+		cwd => '/home/vagrant/',
+		creates => '/home/vagrant/Python-3.4.2.tar.xz',
+		user => vagrant,
+		timeout => 0
+	}->
 	exec{"untar-python":
 		command => 'tar xJf Python-3.4.2.tar.xz',
 		cwd => '/home/vagrant',
