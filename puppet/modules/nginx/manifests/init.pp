42e9790131e33c02fae24b1034365aadac455602
David Parr <dvdprr6@gmail.com>
2014-12-27 18:21:00 -0500

    nginx provisioning

---
+++ puppet/modules/nginx/manifests/init.pp
@@ -0,0 +1,20 @@
+class nginx{
+	# exec{"start-nginx":
+	# 	command => 'service nginx start',
+	# 	cwd => '/home/vagrant',
+	# 	user => root
+	# }->
+	file{"/etc/nginx":
+		ensure => 'directory'
+	}->
+	file{"/etc/nginx/conf.d/bank.conf":
+		ensure => 'link',
+		target => '/vagrant/server/conf/dev/nginx.conf',
+		owner => root
+	}->
+	exec{"restart-nginx":
+		command => 'service nginx start',
+		cwd => '/home/vagrant',
+		user => root
+	}
+}
